c345b7f0ec3d9d8fe6a45b8337790f50c94b86b5
Athulvj <eauditore320@gmail.com>
2021-09-23 15:28:33 +0530

    Parameter practice

--- ComcastSeleniumFramework/src/test/java/com/comcast/contact/ContactTest.java
+++ ComcastSeleniumFramework/src/test/java/com/comcast/contact/ContactTest.java
@@ -5,11 +5,22 @@ import org.testng.annotations.Test;
 public class ContactTest {
 	@Test(groups="SmokeTest")
 	public void createContactTest() {
+		String url = System.getProperty("url");
+		String browser = System.getProperty("browser");
+		
+		System.out.println(url + browser);
+		
 		System.out.println("contact created");
 	}
 	
 	@Test(groups="RegressionTest")
 	public void createContactWithOrgTest() {
+		String url = System.getProperty("url");
+		String browser = System.getProperty("browser");
+		
+		System.out.println(url + browser);
+		
+		System.out.println("contact created");
 		System.out.println("contact created with organization");
 	}
 	
